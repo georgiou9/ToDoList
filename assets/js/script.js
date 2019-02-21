b61d5e49ef08ea792f576ac10a702114bfa9e0ed
oldboy9 <afos3@hotmail.com>
2019-02-20 21:05:05 -0600

    Added functionalities -cross over completed items -delete items

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,13 @@
 
-console.log('script connected')
+// Check off specific todos by clicking
+$('li').on('click', function(){
+    $(this).toggleClass('completed')
+})
+
+// CLick on X to delete Todo
+$('span').on('click', function(event){
+    $(this).parent().fadeOut(600, function(){
+        $(this).remove();
+    });
+    event.stopPropagation(); // stops the bubling effect
+})
