5cccaed5927104581bbcfc0068837e342e6411c1
oldboy9 <afos3@hotmail.com>
2019-02-21 20:44:36 -0600

    Keyboard functionality added -Adding a new task when hiting the Enter key -Freshly added elements inherit event listeners

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,21 @@
 
 // Check off specific todos by clicking
-$('li').on('click', function(){
+$('ul').on('click', 'li', function(){
     $(this).toggleClass('completed')
 })
 
 // CLick on X to delete Todo
-$('span').on('click', function(event){
+$('ul').on('click', 'span', function(event){
     $(this).parent().fadeOut(600, function(){
         $(this).remove();
     });
     event.stopPropagation(); // stops the bubling effect
+})
+
+$("input[type='text']").on('keypress', function(e){
+    if(e.which == 13) {
+        let todoText = $(this).val()
+        $(this).val('')
+        $('ul').append(`<li><span>X</span> ${todoText}</li>`)
+    }
 })
